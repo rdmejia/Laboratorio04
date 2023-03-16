96c997c11d3acba4d35d91e75022904798f37357
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-15 20:14:02 -0600

    Crear ejercicio 02

---
+++ Laboratorio04/Ejercicio02.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio04
+{
+    public class Ejercicio02
+    {
+        public double[] Calcular(int[] nums)
+        {
+            return new double[] {0.0, 0.0};
+        }
+    }
+}
---
+++ Laboratorio04Tests/Ejercicio02Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laboratorio04;
+
+namespace Laboratorio04Tests
+{
+    [TestClass]
+    internal class Ejercicio02Tests
+    {
+        [TestMethod]
+        public void TestSumaYPromedio()
+        {
+
+        }
+    }
+}
