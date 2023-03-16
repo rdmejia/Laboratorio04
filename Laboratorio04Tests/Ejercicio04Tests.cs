cbfbe9834300abf2a2ab22c44ed3ed6ab875cfe1
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-16 14:23:42 -0600

    Solve ejercicio 4

--- Laboratorio04Tests/Ejercicio04Tests.cs
+++ Laboratorio04Tests/Ejercicio04Tests.cs
@@ -6,7 +6,7 @@ namespace Laboratorio04Tests
         [TestMethod]
         [DataRow(347, new int[] {100, 50, 20, 5, 1}, new int[] {3, 0, 2, 1, 2})]
         [DataRow(27, new int[] {1}, new int[] {27})]
-        [DataRow(23, new int[] {23, 1}, new int[] {1, 0})]
+        [DataRow(23, new int[] {23, 1}, new int[] {23, 0})]
         [DataRow(550, new int[] {20, 10, 50, 1}, new int[] {27, 1, 0, 0})]
         public void TestCantidadDeBilletes(int cantidad, int[] billetes, int[] resultado)
         {
@@ -17,7 +17,7 @@ namespace Laboratorio04Tests
             Assert.AreEqual(resultado.Length, resultadoActual.Length, "El resultado no tiene la cantidad de billetes esperada");
 
             for (int i = 0; i < resultado.Length; i++) {
-                Assert.AreEqual(resultado[i], resultadoActual[i], $"El resultado ${i} no es correcto");
+                Assert.AreEqual(resultado[i], resultadoActual[i], $"El resultado {i} no es correcto");
             }
         }
     }
