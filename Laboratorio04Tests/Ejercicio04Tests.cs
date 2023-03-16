1a07ab0a1dd6c526b4dd2bd1b2a851025c244201
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-16 13:58:16 -0600

    Fix ejercicio04tests

--- Laboratorio04Tests/Ejercicio04Tests.cs
+++ Laboratorio04Tests/Ejercicio04Tests.cs
@@ -5,8 +5,8 @@ namespace Laboratorio04Tests
     {
         [TestMethod]
         [DataRow(347, new int[] {100, 50, 20, 5, 1}, new int[] {3, 0, 2, 1, 2})]
-        [DataRow(23, new int[] {1}, new int[] {23})]
-        [DataRow(23, new int[] {23, 1}, new int[] {23, 0})]
+        [DataRow(27, new int[] {1}, new int[] {27})]
+        [DataRow(23, new int[] {23, 1}, new int[] {1, 0})]
         [DataRow(550, new int[] {20, 10, 50, 1}, new int[] {27, 1, 0, 0})]
         public void TestCantidadDeBilletes(int cantidad, int[] billetes, int[] resultado)
         {
