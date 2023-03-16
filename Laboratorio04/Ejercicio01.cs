9b5252bf0675e422bbd5b5bd5b9352a7788234e4
Daniel Mejia <rdmejiaa@gmail.com>
2023-03-15 19:05:31 -0600

    Create Ejercicio01Tests

---
+++ Laboratorio04/Ejercicio01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio04
+{
+    public class Ejercicio01
+    {
+        public static int Sumar(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Laboratorio04Tests/Ejercicio01Tests.cs
@@ -0,0 +1,30 @@
+
+namespace Laboratorio04Tests
+{
+    using Laboratorio04;
+
+    [TestClass]
+    public class Ejercicio01Tests
+    {
+        [TestMethod]
+        public void TestSumar()
+        {
+            int result = Ejercicio01.Sumar(4, 3);
+            Assert.AreEqual(7, result); 
+        }
+
+        [TestMethod]
+        public void TestSumar2()
+        {
+            int result = Ejercicio01.Sumar(0, 0);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod] 
+        public void TestSumar3() 
+        {
+            int result = Ejercicio01.Sumar(-1, -1);
+            Assert.AreEqual(-2, result); 
+        }
+    }
+}
